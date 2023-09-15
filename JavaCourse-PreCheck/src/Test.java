f16c6e8cc2e3c08feb27d99374a135bf06091ea4
Oğuzcan Şencan <oguzcansencan@gmail.com>
2023-09-15 22:15:08 +0200

    Update Test.java

--- JavaCourse-PreCheck/src/Test.java
+++ JavaCourse-PreCheck/src/Test.java
@@ -23,14 +23,14 @@ public class Test {
 				p8_nationalAwards 	= new ArrayList<String>(Arrays.asList("National Award #9")),
 				p8_internationalAwards 	= new ArrayList<String>(Arrays.asList("International Award #10"));
 		
-		Participant p1 = new Participant("name1", "surname1", 24, "Basketball", p1_nationalAwards, p1_internationalAwards),
-					p2 = new Participant("name2", "surname2", 22, "Basketball", p2_nationalAwards, p2_internationalAwards),
-					p3 = new Participant("name3", "surname3", 26, "Basketball", p3_nationalAwards, p3_internationalAwards),
-					p4 = new Participant("name4", "surname4", 25, "Football", p4_nationalAwards, p4_internationalAwards),
-					p5 = new Participant("name5", "surname5", 25, "Tennis", p5_nationalAwards, p5_internationalAwards),
-					p6 = new Participant("name6", "surname6", 26, "Tennis", p6_nationalAwards, p6_internationalAwards),
-					p7 = new Participant("name7", "surname7", 21, "Football", p7_nationalAwards, p7_internationalAwards),
-					p8 = new Participant("name8", "surname8", 20, "Baseball", p8_nationalAwards, p8_internationalAwards);
+		Participant 	p1 = new Participant("name1", "surname1", 24, "Basketball", p1_nationalAwards, p1_internationalAwards),
+				p2 = new Participant("name2", "surname2", 22, "Basketball", p2_nationalAwards, p2_internationalAwards),
+				p3 = new Participant("name3", "surname3", 26, "Basketball", p3_nationalAwards, p3_internationalAwards),
+				p4 = new Participant("name4", "surname4", 25, "Football", p4_nationalAwards, p4_internationalAwards),
+				p5 = new Participant("name5", "surname5", 25, "Tennis", p5_nationalAwards, p5_internationalAwards),
+				p6 = new Participant("name6", "surname6", 26, "Tennis", p6_nationalAwards, p6_internationalAwards),
+				p7 = new Participant("name7", "surname7", 21, "Football", p7_nationalAwards, p7_internationalAwards),
+				p8 = new Participant("name8", "surname8", 20, "Baseball", p8_nationalAwards, p8_internationalAwards);
 		
 		Participant[] arr_participants = {p1, p2, p3, p4, p5, p6, p7, p8};
 		
